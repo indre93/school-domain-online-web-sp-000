dafa26ea85759bcad52c7e6113010731b5a2caa4
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-21 03:28:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 # code here!
 
 class School
-  attr_accessor 
+  attr_accessor
   attr_reader :name, :roster
 
   def initialize(name)
@@ -9,7 +9,7 @@ class School
     @roster = {}
   end
 
-  def roster 
+  def roster
     @roster
   end
 
